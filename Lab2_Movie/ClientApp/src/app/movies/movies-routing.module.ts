b5ecb4403d7e6081a5e5835aed0bd1b4b8a081ec
noemik <tucita14@yahoo.com>
2020-06-15 17:03:34 +0300

    movies-routing module added

--- Lab2_Movie/ClientApp/src/app/movies/movies-routing.module.ts
+++ Lab2_Movie/ClientApp/src/app/movies/movies-routing.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { MoviesEditComponent } from './movies-edit/movies-edit.component';
 import { MoviesListComponent } from './movies-list/movies-list.component';
+import { Routes, RouterModule } from '@angular/router';
+import { MoviesComponent } from './movies.component';
 
+
+const routes: Routes = [
+    {
+        path: '', component: MoviesComponent,
+        children: [
+            { path: '', redirectTo: 'list', pathMatch: 'full' },
+            { path: 'list', component: MoviesListComponent },
+            { path: 'edit/:id', component: MoviesEditComponent },
+            { path: 'edit', component: MoviesEditComponent },
+        ]
+    }
+];
 
 
 @NgModule({
-  declarations: [MoviesEditComponent, MoviesListComponent],
-  imports: [
-    CommonModule
-  ]
+  //declarations: [MoviesEditComponent, MoviesListComponent],
+  //imports: [
+  //  CommonModule
+  //]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
-export class MoviesModule { }
+export class MoviesRoutingModule {
+
+    static routedComponents = [MoviesComponent, MoviesListComponent, MoviesEditComponent];
+
+}
