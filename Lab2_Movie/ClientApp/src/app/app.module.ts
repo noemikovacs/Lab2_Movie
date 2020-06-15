32aec1481f7c1856915846d60d47849f7b12cd75
noemik <tucita14@yahoo.com>
2020-06-15 16:15:13 +0300

    set shared folder and added AngularMaterialModule

--- Lab2_Movie/ClientApp/src/app/app.module.ts
+++ Lab2_Movie/ClientApp/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
+import { AngularMaterialModule } from './shared/angular-material.module';
+
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
@@ -30,8 +32,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
     ]),
-    BrowserAnimationsModule
-  ],
+      BrowserAnimationsModule,
+      AngularMaterialModule
+    ],
+    exports: [AngularMaterialModule],
   providers: [],
   bootstrap: [AppComponent]
 })
